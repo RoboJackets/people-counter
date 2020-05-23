dc9841b37bcacf4dc78745c3a7dd23ba45f5a12d
Ross Lindsay <ross@rosslindsay.com>
2020-05-23 13:44:07 -0400

    Initial roles and permissions

---
+++ database/migrations/2020_05_23_154904_create_initial_roles_permissions.php
@@ -0,0 +1,80 @@
+<?php
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInitialRolesPermissions extends Migration
+{
+    public $allPermissions = [
+        'manage-users',
+        'manage-visits',
+        'create-visits',
+        'create-visits-own',
+        'read-visits',
+        'read-visits-own',
+        'update-visits',
+        'update-visits-own',
+        'delete-visits',
+        'delete-visits-own'
+    ];
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        global $allPermissions;
+
+        // Reset cached roles and permissions
+        app()['cache']->forget('spatie.permission.cache');
+
+        // Create Permissions
+        foreach ($allPermissions as $vp) {
+            Permission::firstOrCreate(['name' => $vp]);
+        }
+
+        $superadmin = Role::firstOrCreate(['name' => 'super-admin']);
+
+        $member = Role::firstOrCreate(['name' => 'member']);
+        $memberRoles = [
+            'create-visits-own',
+            'read-visits-own',
+            'update-visits-own',
+            'delete-visits-own'
+        ];
+        $member->syncPermissions($memberRoles);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        global $allPermissions;
+
+        // Reset cached roles and permissions
+        app()['cache']->forget('spatie.permission.cache');
+
+        $roles = ['super-admin', 'member'];
+        foreach ($roles as $role) {
+            $dbRole = Role::where('name', $role)->first();
+            if (null === $dbRole) {
+                continue;
+            }
+            $dbRole->delete();
+        }
+
+        foreach ($allPermissions as $permission) {
+            $dbPerm = Permission::where('name', $permission)->first();
+            if (null === $dbPerm) {
+                continue;
+            }
+            $dbPerm->delete();
+        }
+    }
+}
