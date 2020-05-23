0217a612f33219ba9cc8ac6733b03ee0e02f9a05
Ross Lindsay <ross@rosslindsay.com>
2020-05-23 13:46:37 -0400

    Routes for users/visits

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', static function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('users', 'UserController');
+Route::apiResource('visits', 'VisitController');
