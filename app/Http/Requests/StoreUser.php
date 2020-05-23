c8dc5c816ebaac0c19090f22f751c940e1a539ec
Ross Lindsay <ross@rosslindsay.com>
2020-05-23 13:45:02 -0400

    HTTP Requests

---
+++ app/Http/Requests/StoreUser.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUser extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // Handled in Policy
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username' => 'required,unique:App\User',
+            'email' => 'required,unique:App\User,email',
+            'first_name' => 'required',
+            'last_name' => 'required'
+        ];
+    }
+}
