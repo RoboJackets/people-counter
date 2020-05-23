17161f5518e94571bc08e528fe35e050f15caa68
Ross Lindsay <ross@rosslindsay.com>
2020-05-23 11:31:54 -0400

    Initial migrations

--- database/migrations/2020_05_23_150341_create_visits_table.php
+++ database/migrations/2020_05_23_150341_create_visits_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class VisitsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('visits', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->integer('gtid');
+            $table->timestamp('in_time');
+            $table->timestamp('out_time')->nullable();
+            $table->string('door');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('visits');
     }
 }
