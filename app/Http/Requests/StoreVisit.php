aa5ee2003106bfbd770cc6c8cebc643c6d3fbeb9
Ross Lindsay <ross@rosslindsay.com>
2020-05-24 14:52:49 -0400

    Rename to fit convention

--- app/Http/Requests/StoreVisit.php
+++ app/Http/Requests/StoreVisit.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreateVisit extends FormRequest
+class StoreVisit extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
