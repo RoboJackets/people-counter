2be555288b997c40fa16d2cd97a703be61137e8b
Ross Lindsay <ross@rosslindsay.com>
2020-05-24 20:20:35 -0400

    Need in and out door, not just one door

---
+++ database/migrations/2020_05_24_225537_add_second_door_to_visits.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSecondDoorToVisits extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('visits', function (Blueprint $table) {
+            $table->renameColumn('door', 'in_door');
+            $table->string('out_door')->after('door')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('visits', function (Blueprint $table) {
+            $table->dropColumn('out_door');
+            $table->renameColumn('in_door', 'door');
+        });
+    }
+}
