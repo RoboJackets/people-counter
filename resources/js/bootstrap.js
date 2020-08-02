5f104994ebf27fa634138954e8aa64397b6a1838
Ross Lindsay <ross@rosslindsay.com>
2020-08-02 16:44:35 -0400

    Default use cookie for auth from Sanctum

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,6 +10,7 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
 
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
