ace512906a47d530bac599447116f3bb09dc7e45
Ross Lindsay <ross@rosslindsay.com>
2020-08-13 22:49:25 -0400

    Started building out SpaceController

--- app/Http/Controllers/SpaceController.php
+++ app/Http/Controllers/SpaceController.php
@@ -2,26 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Space as SpaceResource;
 use App\Space;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class SpaceController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection<SpaceResource>
      */
     public function index()
     {
-        //
+        $spaces = QueryBuilder::for(Space::class)
+            ->allowedFilters(
+                [
+                    AllowedFilter::exact('id'),
+                    'name',
+                    'max_occupancy'
+                ]
+            )
+            ->allowedSorts('name')
+            ->allowedIncludes(['parent', 'children'])
+            ->get();
+
+        return SpaceResource::collection($spaces);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
@@ -32,7 +48,7 @@ class SpaceController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Space  $space
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show(Space $space)
     {
@@ -44,7 +60,7 @@ class SpaceController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Space  $space
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, Space $space)
     {
@@ -55,7 +71,7 @@ class SpaceController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  \App\Space  $space
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Space $space)
     {
