cd603c0c50b61b916aef2eb7731c39c1c49e4951
Ross Lindsay <ross@rosslindsay.com>
2020-08-22 22:29:33 -0400

    Always include computed full name

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -26,6 +26,7 @@ class User extends JsonResource
             'email' => $this->email,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
+            'full_name' => $this->full_name,
             'gtid' => $this->when($this->shouldIncludeGTID($request), $this->gtid),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
