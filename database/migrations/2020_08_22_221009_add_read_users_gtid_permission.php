193c7704eec2b55b94189481527da3aa7939c975
Ross Lindsay <ross@rosslindsay.com>
2020-08-22 22:28:29 -0400

    Add permission for reading GTID

---
+++ database/migrations/2020_08_22_221009_add_read_users_gtid_permission.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Spatie\Permission\Models\Permission;
+
+class AddReadUsersGtidPermission extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Reset cached roles and permissions
+        app()['cache']->forget('spatie.permission.cache');
+
+        Permission::firstOrCreate(['name' => 'read-users-gtid']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $dbPerm = Permission::where('name', 'read-users-gtid')->first();
+
+        if (null === $dbPerm) {
+            return;
+        }
+
+        $dbPerm->delete();
+    }
+}
