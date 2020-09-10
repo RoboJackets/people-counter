82977cc179dd83756dffa76fb82832dbed5cabcb
Josh Oldenburg <JoshOldenburg@users.noreply.github.com>
2020-09-09 20:58:38 -0400

    Fix Nova footer GitHub link

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a class="text-primary dim no-underline" href="https://github.com/RoboJackets/apiary">Made with ♥ by RoboJackets</a>
+    <a class="text-primary dim no-underline" href="https://github.com/RoboJackets/people-counter">Made with ♥ by RoboJackets</a>
     <span class="px-1">&middot;</span>
     <a class="text-primary dim no-underline" class="text-muted" href="/privacy">Privacy Policy</a>
     <span class="px-1">&middot;</span>
