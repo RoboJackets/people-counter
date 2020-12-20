ad1a8749cc1e6cb1e67dec4c67bf5463977d67e5
Laravel Shift <shift@laravelshift.com>
2020-12-20 17:48:55 +0000

    Shift to class based factories

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -24,6 +25,8 @@ use Spatie\Permission\Traits\HasRoles;
  */
 class User extends Authenticatable
 {
+    use HasFactory;
+
     use HasApiTokens;
     use HasRoles;
     use Notifiable;
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,8 +8,10 @@ declare(strict_types=1);
  * @var \Illuminate\Database\Eloquent\Factory $factory
  */
 
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
 use App\User;
-use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 /*
@@ -23,12 +25,28 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(User::class, static function (Faker $faker): array {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-    ];
-});
+class UserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ];
+    }
+}
