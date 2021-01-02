fe8d0bad2820b0e024c71a3fc2a9a661192c35d7
Ross Lindsay <ross@rosslindsay.com>
2021-01-02 16:20:44 -0500

    Missing param

--- resources/js/components/Kiosk.vue
+++ resources/js/components/Kiosk.vue
@@ -288,7 +288,7 @@ export default {
                 })
                 .catch(error => {
                     this.axiosErrorToBugsnag(error)
-                    this.handleAxiosError()
+                    this.handleAxiosError(error)
                 });
         },
         async loadSpaces() {
