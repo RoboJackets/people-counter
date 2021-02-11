d849d1ca32133eed2eca44ffc7e93e5134f20b2b
Ross Lindsay <ross@rosslindsay.com>
2021-02-10 22:11:49 -0500

    Improved style of footer elements

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,9 @@
-<footer class="footer">
-  <div class="container pt-4">
-    <div class="d-flex justify-content-between">
-      <div>
-        <p class="text-muted text-right"><a class="text-muted" href="https://github.com/RoboJackets/people-counter">Made with ♥ by RoboJackets</a> • <a class="text-muted" href="https://www.gatech.edu/privacy">Privacy Policy</a></p>
-      </div>
-      <div>
-        <a class="text-muted text-left" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSelERsYq3gLmHbWvVCWha5iCU8z3r9VYC0hCN4ArLpMAiysaQ/viewform?entry.1338203640={{ request()->fullUrl()}}">Make a Wish</a>
-      </div>
-    </div>
-  </div>
+<footer class="container pt-4 text-center text-small">
+    <p class="mb-1">
+        <a class="text-muted" href="https://github.com/RoboJackets/people-counter">Made with ♥ by RoboJackets</a>
+    </p>
+    <ul class="list-inline">
+        <li class="list-inline-item"><a class="text-muted" href="https://www.gatech.edu/privacy">Privacy Policy</a></li>
+        <li class="list-inline-item"><a class="text-muted" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSelERsYq3gLmHbWvVCWha5iCU8z3r9VYC0hCN4ArLpMAiysaQ/viewform?entry.1338203640={{ request()->fullUrl()}}">Make a Wish</a></li>
+    </ul>
 </footer>
