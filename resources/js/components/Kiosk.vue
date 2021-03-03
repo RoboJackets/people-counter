dd17e49d5d8a2526cb8a41f0cb5c0001200a84b4
Ross Lindsay <ross@rosslindsay.com>
2021-03-02 23:54:52 -0500

    Phrasing

--- resources/js/components/Kiosk.vue
+++ resources/js/components/Kiosk.vue
@@ -505,7 +505,7 @@ export default {
                         this.axiosErrorToBugsnag(error)
                         this.$swal.fire({
                             title: 'Error',
-                            text: 'An unexpected error occurred. If this continues, post in Slack or contact developers@robojackets.org.',
+                            text: 'An unexpected error occurred. If this continues, post in SCC/RJ Slack #people-counter or contact developers@robojackets.org.',
                             icon: 'error',
                             timer: 5000,
                             timerProgressBar: true,
@@ -543,7 +543,7 @@ export default {
             } else {
                 this.$swal.fire(
                     'Error',
-                    'An unexpected error occurred. If this continues, post in Slack or contact developers@robojackets.org.',
+                    'An unexpected error occurred. If this continues, post in SCC/RJ Slack #people-counter or contact developers@robojackets.org.',
                     'error'
                 );
             }
