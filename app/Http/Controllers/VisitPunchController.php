74e33da10a9b048403d3cd23731567ad318ba226
Ross Lindsay <ross@rosslindsay.com>
2021-03-20 14:28:23 -0400

    Additional logic based on affiliation for users without space associations

--- app/Http/Controllers/VisitPunchController.php
+++ app/Http/Controllers/VisitPunchController.php
@@ -127,9 +127,25 @@ class VisitPunchController extends Controller
         // Check for default space for user
         $userSpaces = $user->spaces;
         if (0 === count($userSpaces)) {
-            // Query SUMS if we have a valid user
-            $updatedSpaces = collect();
-            if (null !== $user) {
+            $updatedSpaces = null;
+            if (null === $user->primary_affiliation) {
+                // Fetch primary affiliation from BuzzAPI for any user who doesn't have one
+                try {
+                    $user = $this->createOrUpdateUserFromBuzzAPI($gtid, false, true);
+                } catch (\Throwable $e) {
+                    Log::error(
+                        'Error querying BuzzAPI to update user for punch by '.$gtid,
+                        [$e->getMessage()]
+                    );
+
+                    return response()->json([
+                        'status' => 'error',
+                        'error' => 'Unable to update user due to BuzzAPI failure',
+                    ], 500);
+                }
+            }
+            if ('student' === $user->primary_affiliation && null !== $user) {
+                // Query SUMS if we have a valid user who is also a student
                 $updatedSpaces = $this->updateUserSpacesFromSUMS($user);
             }
             if (null === $updatedSpaces) {
