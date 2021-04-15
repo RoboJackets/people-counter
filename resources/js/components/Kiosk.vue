8d7c452fbca15ae4fd841b3f0dcca97ac527f529
Evan Strat <5790137+evan10s@users.noreply.github.com>
2021-04-15 15:13:54 -0400

    Poke concourse, part 2

--- resources/js/components/Kiosk.vue
+++ resources/js/components/Kiosk.vue
@@ -447,6 +447,7 @@ export default {
                     self.wsConnectionOk = true;
                     self.offlineCardSwipes = 0;
                     self.wsConnectionFailedAt = null;
+
                     // Reload data that might have changed since coming back online
                     self.loadUser();
                     self.loadSpace();
